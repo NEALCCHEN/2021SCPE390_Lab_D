7918f976b4fee9fe5717d0575560d22b6383a61b
MINGXUAN CHEN <mingxuanchen@MINGXUANdeMacBook-Pro-2.local>
2021-03-10 17:58:03 -0500

    add Lab6

---
+++ Lab6/functions.cc
@@ -0,0 +1,19 @@
+int setBit(int a, int b){
+
+}
+
+int clearBit(int a, int b){
+
+}
+
+int flip(int a, int b){
+
+}
+
+int replaceBits(int a, int b, int c){
+
+}
+
+int buildColor(int a, int b, int c){
+
+}
---
+++ Lab6/lab6.s
@@ -0,0 +1,22 @@
+	.globl _Z6setBitii
+_Z6setBitii:
+
+
+
+	.globl _Z8clearBitii
+_Z8clearBitii:
+
+
+
+	.globl _Z4flipii
+_Z4flipii:
+
+
+
+	.globl _Z11replaceBitsiii
+_Z11replaceBitsiii:
+
+
+
+	.globl _Z10buildColoriii
+_Z10buildColoriii:
